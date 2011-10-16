d3f324a91ec72495cc1c2390cc693c49a675e68b
Christian Trutz <christian.trutz@gmail.com>
2011-10-16 22:43:43 +0200

    Table columns are visible now.
    Signed-off-by: Christian Trutz <christian.trutz@gmail.com>

--- logback.mongodb.client/src/logback/mongodb/client/LogsLabelProvider.java
+++ logback.mongodb.client/src/logback/mongodb/client/LogsLabelProvider.java
@@ -4,6 +4,8 @@ import org.eclipse.jface.viewers.ITableLabelProvider;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.swt.graphics.Image;
 
+import com.mongodb.DBObject;
+
 public class LogsLabelProvider extends LabelProvider implements
 		ITableLabelProvider {
 
@@ -14,6 +16,23 @@ public class LogsLabelProvider extends LabelProvider implements
 
 	@Override
 	public String getColumnText(Object element, int columnIndex) {
+		if (element instanceof DBObject) {
+			DBObject log = (DBObject) element;
+			switch (columnIndex) {
+			case 0:
+				return log.get("message").toString();
+			case 1:
+				return log.get("logger").toString();
+			case 2:
+				return log.get("thread").toString();
+			case 3:
+				return log.get("timestamp").toString();
+			case 4:
+				return log.get("level").toString();
+			default:
+				break;
+			}
+		}
 		return getText(element);
 	}
 
--- logback.mongodb.client/src/logback/mongodb/client/LogsView.java
+++ logback.mongodb.client/src/logback/mongodb/client/LogsView.java
@@ -4,6 +4,8 @@ import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.ui.part.ViewPart;
 
 public class LogsView extends ViewPart {
@@ -16,6 +18,35 @@ public class LogsView extends ViewPart {
 		root.setLayout(new FillLayout());
 		logs = new TableViewer(root, SWT.VIRTUAL | SWT.MULTI | SWT.H_SCROLL
 				| SWT.V_SCROLL | SWT.BORDER);
+
+		Table logsTable = logs.getTable();
+		logsTable.setHeaderVisible(true);
+
+		TableColumn column = new TableColumn(logsTable, SWT.NONE);
+		column.setText("message");
+		column.setWidth(300);
+		column.setMoveable(true);
+
+		column = new TableColumn(logsTable, SWT.NONE);
+		column.setText("logger");
+		column.setWidth(100);
+		column.setMoveable(true);
+
+		column = new TableColumn(logsTable, SWT.NONE);
+		column.setText("thread");
+		column.setWidth(100);
+		column.setMoveable(true);
+
+		column = new TableColumn(logsTable, SWT.NONE);
+		column.setText("timestamp");
+		column.setWidth(100);
+		column.setMoveable(true);
+
+		column = new TableColumn(logsTable, SWT.NONE);
+		column.setText("level");
+		column.setWidth(50);
+		column.setMoveable(true);
+
 		logs.setContentProvider(new LogsContentProvider(logs));
 		logs.setLabelProvider(new LogsLabelProvider());
 		logs.setItemCount((int) MongoConnection.getDBCollection().count());
